e5b8f7b7a53ea453ba8c9881a1dffc38df974975
Amber Swope <abemily93@gmail.com>
2021-03-21 02:43:19 +0000

    wrote the tests and then the code

--- message.js
+++ message.js
@@ -1,12 +1,11 @@
 class Message {
-  constructor(messageName,command){
-    this.messageName = messageName;
-    if(!messageName){
-      throw Error ("Message name required")
-    }
-    this.command = command
-  }
-
+   constructor(name, commands){
+     if(!name){
+       throw Error('Message name required.');
+     }
+     this.name = name;
+     this.commands = commands;
+   }
 }
 
 module.exports = Message;
